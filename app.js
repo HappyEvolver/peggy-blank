4c8e70f6de14c423be77f3584048fab14744be64
Neil <neil@bigfoot-computing.co.uk>
2013-07-29 07:25:36 -0400

    Added extra pages and Twitter Bootstrap

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get("/", function(req, res) {
   });
 });
 
-// route for page two
+// route for Zopim demo
 app.get("/zopim", function(req, res) {
   fs.readFile(__dirname + '/zopim.html',
   function (err, data) {
@@ -38,12 +38,36 @@ app.get("/zopim", function(req, res) {
   });
 });
 
-// route for page three
+// route for PureChat demo
 app.get("/purechat", function(req, res) {
   fs.readFile(__dirname + '/purechat.html',
   function (err, data) {
     if (err) {
       return res.send(500, 'Error loading purechat.html');
+    }
+    res.set('Content-Type', 'text/html');
+    res.send(200, data);
+  });
+});
+
+// route for P3Chat demo
+app.get("/p3chat", function(req, res) {
+  fs.readFile(__dirname + '/p3chat.html',
+  function (err, data) {
+    if (err) {
+      return res.send(500, 'Error loading p3chat.html');
+    }
+    res.set('Content-Type', 'text/html');
+    res.send(200, data);
+  });
+});
+
+// route for ClickDesk demo
+app.get("/clickdesk", function(req, res) {
+  fs.readFile(__dirname + '/clickdesk.html',
+  function (err, data) {
+    if (err) {
+      return res.send(500, 'Error loading clickdesk.html');
     }
     res.set('Content-Type', 'text/html');
     res.send(200, data);
